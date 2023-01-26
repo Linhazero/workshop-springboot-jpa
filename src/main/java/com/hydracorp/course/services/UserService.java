afdff43b781ae208afa78cd9bea5f5a60b23b1d9
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-01-26 10:06:24 -0300

    Exception handling - findById

--- src/main/java/com/hydracorp/course/services/UserService.java
+++ src/main/java/com/hydracorp/course/services/UserService.java
@@ -3,12 +3,12 @@ package com.hydracorp.course.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hydracorp.course.entities.User;
 import com.hydracorp.course.repositories.UserRepository;
+import com.hydracorp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
