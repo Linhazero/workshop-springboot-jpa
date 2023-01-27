821982bc2229fd7181fb459179601efb82361c41
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-01-27 11:31:57 -0300

    Exception handling - update

--- src/main/java/com/hydracorp/course/services/UserService.java
+++ src/main/java/com/hydracorp/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.hydracorp.course.repositories.UserRepository;
 import com.hydracorp.course.services.exceptions.DatabaseException;
 import com.hydracorp.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
